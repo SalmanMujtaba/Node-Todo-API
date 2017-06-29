1698e2cee607ca6ea08a035436696ee15c9e7805
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-06-29 13:08:18 -0400

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,24 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('environment:'+env);
-if(env==='development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}
 
-else if(env==='test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoTest';
+if(env === 'development' || env === 'test' || env ==='production')
+{
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key)=>{
+    process.env[key] = envConfig[key];
+  });
 }
-else if(env==='production'){
-  process.env.MONGODB_URI = 'mongodb://salman:1234@ds135382.mlab.com:35382/sample';
-}
+// if(env==='development'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }
+//
+// else if(env==='test'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoTest';
+// }
+// else if(env==='production'){
+//   process.env.MONGODB_URI = 'mongodb://salman:1234@ds135382.mlab.com:35382/sample';
+// }
